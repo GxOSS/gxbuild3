d1222476dd4591b3bc915f785ea5647fbe891ae2
erorn <erorndev@proton.me>
2026-06-21 20:34:59 +0300

    Update entry point

--- src/GxBuild3.cpp
+++ src/GxBuild3.cpp
@@ -3,11 +3,22 @@
 
 #include <iostream>
 
+#if defined(_MSC_VER)
+constexpr const char* compiler = "MSVC";
+#elif defined(__clang__)
+constexpr const char* compiler = "Clang";
+#elif defined(__GNUC__)
+constexpr const char* compiler = "GCC";
+#else
+constexpr const char* compiler = "Unknown";
+#endif
+
 int main() {
     Log::Init();
 
     std::cout << Ascii::Logo;
 
-    Log::Info("hello from GxBuild3.");
+    Log::Info("gxbuild3 starting... (Built on: {} {} with {})", __DATE__, __TIME__, compiler);
+
     return 0;
 }
