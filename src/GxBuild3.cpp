391b85d7ed1f7946892aad8cef4e6d276b08cb36
erorn <erorndev@proton.me>
2026-06-22 10:22:36 +0300

    CLI11 integration

--- src/GxBuild3.cpp
+++ src/GxBuild3.cpp
@@ -1,6 +1,9 @@
 #include "Ascii.hpp"
 #include "Log.hpp"
 
+#include <CLI/App.hpp>
+#include <CLI/Config.hpp>
+#include <CLI/Formatter.hpp>
 #include <iostream>
 
 #if defined(_MSC_VER)
@@ -13,12 +16,20 @@ constexpr const char* compiler = "GCC";
 constexpr const char* compiler = "Unknown";
 #endif
 
-int main() {
+int main(int argc, char** argv) {
     Log::Init();
 
     std::cout << Ascii::Logo;
 
     Log::Info("gxbuild3 starting... (Built on: {} {} with {})", __DATE__, __TIME__, compiler);
+
+    CLI::App app{"GxBuild3"};
+
+    try {
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError& e) {
+        return app.exit(e);
+    }
 
     return 0;
 }
